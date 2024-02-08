3bb5b1e3833d1a70d1f92cff9c1b1205443ede99
DarkRoseVi <viktorijajazva@gmail.com>
2024-02-08 14:40:01 +0300

    Добавьте файлы проекта.

---
+++ Vector/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Vector;
+
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("x");
+//double x = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("y");
+//double y = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("z");
+//double z = Convert.ToDouble(Console.ReadLine());
+VectorClass vector = new VectorClass(1,2,3);
+vector.Print();
+
+
+//Console.WriteLine("x2");
+//double x2 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("y2");
+//double y2 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine("z2");
+//double z2 = Convert.ToDouble(Console.ReadLine());
+VectorClass vector2 = new VectorClass(4, 5, 6);
+vector2.Print();
+VectorClass sums = vector.AddVector(vector2);
+sums.Print();
+VectorClass difference = vector.Subtract(vector2);
+difference.Print();
+Console.WriteLine($"Length of vector1:{vector.Length:0.00} " ); Console.WriteLine($"Length of sum:{sums.Length: 0.00} " );
+Console.WriteLine($"Length of difference: {difference.Length: 0.00}" );
+
+
+
+Console.ReadKey();  
---
+++ Vector/VectorClass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector
+{
+    internal class VectorClass
+    {
+        public  double x;
+        public double y;
+        public double z;
+        public VectorClass(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+      public  double Length => Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+      
+        public void PrintLenth() 
+        {
+            Console.WriteLine("Length " + Length);
+        }
+        public void Print()
+        {
+            Console.WriteLine(x + " "+ y + " " + z);
+        }
+
+        public  VectorClass AddVector ( VectorClass vector2)
+        {
+            double sumX = x + vector2.x;
+            double sumY = y + vector2.y;
+            double sumZ = z + vector2.z;
+            return new VectorClass(sumX, sumY,sumZ);
+        }
+        public VectorClass Subtract(VectorClass vector2)
+        {
+            double sumX = x - vector2.x;
+            double sumY = y - vector2.y;
+            double sumZ = z - vector2.z;
+            return new VectorClass(sumX, sumY, sumZ);
+        }
+    }
+    
+}
